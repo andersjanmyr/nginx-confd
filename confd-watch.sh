21171882e4aa9818d5bf7f2a86738e551e5769c7
Anders Janmyr <anders@janmyr.com>
2015-03-29 21:58:40 +0200

    Fixed the default value

--- confd-watch.sh
+++ confd-watch.sh
@@ -3,18 +3,18 @@
 # Fail hard and fast
 set -eo pipefail
 
-NODE=${CONFD_NODE:-172.17.42.1:4001}
+node=${CONFD_NODE:-172.17.42.1:4001}
 
-echo "[nginx] booting container. CONFD_NODE: $NODE"
+echo "[nginx] booting container. CONFD_NODE: $node"
 
 # Loop until confd has updated the nginx config
-until confd -onetime -node $NODE -config-file /etc/confd/conf.d/nginx.toml; do
+until confd -onetime -node $node -config-file /etc/confd/conf.d/nginx.toml; do
   echo "[nginx] waiting for confd to refresh nginx.conf"
   sleep 5
 done
 
 # Run confd in the background to watch the upstream servers
-confd -interval 10 -node $NODE -config-file /etc/confd/conf.d/nginx.toml &
+confd -interval 10 -node $node -config-file /etc/confd/conf.d/nginx.toml &
 echo "[nginx] confd is listening for changes on etcd..."
 
 # Start nginx
