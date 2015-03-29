5fcc9fdf2e72fe31c1d62c1a63f64621a9f655b2
Anders Janmyr <anders@janmyr.com>
2015-03-29 18:37:49 +0200

    Full path to confd

--- confd-watch.sh
+++ confd-watch.sh
@@ -8,7 +8,7 @@ export CONFD_NODE=${CONFD_NODE}:172.17.42.1:4001
 echo "[nginx] booting container. CONFD_NODE: $CONFD_NODE"
 
 # Loop until confd has updated the nginx config
-until confd -onetime -node $CONFD_NODE -config-file /etc/confd/conf.d/nginx.toml; do
+until /usr/local/bin/confd -onetime -node $CONFD_NODE -config-file /etc/confd/conf.d/nginx.toml; do
   echo "[nginx] waiting for confd to refresh nginx.conf"
   sleep 5
 done
